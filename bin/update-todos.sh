d7d009c7a126a66a16ce97f9d1c79063cbf28f18
Eric Bowman <bowmane@dirac.localdomain>
2021-05-17 13:56:55 +0200

    First checkin

---
+++ bin/update-todos.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+source /etc/zshrc
+source /Users/bowmane/.zshrc
+md-open-items.scala "/Users/bowmane/OneDrive - TomTom/Notes"
+
---
+++ src/main/scala/ie/boboco/md/Main.scala
@@ -0,0 +1,58 @@
+package ie.boboco.md
+
+import java.io.{File, FileWriter, PrintWriter}
+import java.text.SimpleDateFormat
+import java.util.Date
+import scala.io.Source
+import scala.language.{implicitConversions, reflectiveCalls}
+
+trait Utils {
+  def using[A, B <: {def close(): Unit}](closeable: B)(f: B => A): A =
+    try {
+      f(closeable)
+    } finally {
+      closeable.close()
+    }
+}
+
+object Main extends App with Utils {
+
+  val prefix = "000 TODOs - "
+  val dir = new File(args(0))
+  val todos = findMarkdownFiles(dir).filter(containsUncompleted).map(getUncompleted)
+
+  writeTodos(todos)
+
+  def writeTodos(todos: Seq[(File, Seq[String])]): Unit = {
+    val sdf = new SimpleDateFormat("yyyy-MM-dd")
+    val fileName: String = s"$prefix${sdf.format(new Date())}.md"
+    val destFile = new File(dir.getAbsolutePath, fileName)
+    using(new PrintWriter(new FileWriter(destFile))) { writer =>
+      writer.println(s"# Open TODOs")
+      for (todo <- todos) {
+        writer.println("\n")
+        writer.println(s"## [[${todo._1.getName.replaceAll("\\.md", "")}]]")
+        writer.println("\n")
+        for (line <- todo._2) {
+          writer.println(line)
+        }
+      }
+    }
+  }
+
+  def findMarkdownFiles(dir: File): List[File] = {
+    assert(dir.isDirectory, s"$dir is not a directory")
+    val mdThisDir = dir.listFiles { pathname =>
+      pathname.isFile && pathname.getName.endsWith(".md") && !pathname.getName.startsWith(prefix)
+    }.toList.sortBy(_.getName)
+    mdThisDir ::: dir.listFiles(_.isDirectory).toList.flatMap(d => findMarkdownFiles(d))
+  }
+
+  def containsUncompleted(f: File): Boolean = using(Source.fromFile(f)) { buffered =>
+    buffered.getLines().exists(_.contains("[ ]"))
+  }
+
+  def getUncompleted(f: File): (File, Seq[String]) = using(Source.fromFile(f)) { buffered =>
+    (f, buffered.getLines().filter(_.contains("[ ]")).toList)
+  }
+}
